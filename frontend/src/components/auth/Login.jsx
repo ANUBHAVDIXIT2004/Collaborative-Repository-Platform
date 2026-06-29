ec1f687fc0b656e30d23e70667f662c3d484cab1
Anubhav Dixit <anubhavdixit1362012@gmail.com>
2026-06-29 11:25:20 +0530

    handle logos and name

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -98,7 +98,7 @@ const Login = () => {
         </div>
         <div className="pass-box">
           <p>
-            New to GitHub? <Link to="/signup">Create an account</Link>
+            New to BitHub? <Link to="/signup">Create an account</Link>
           </p>
         </div>
       </div>
